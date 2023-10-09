92c26aea20db93f68281ce2533c25e3bd1eedaa5
Kinleyjigs <141105750+Kinleyjigs@users.noreply.github.com>
2023-10-09 22:50:31 +0600

    python task 75

---
+++ #Task 8.py
@@ -0,0 +1,11 @@
+#Task 8
+#Create a variable called age of type int and assign the value 18 to the variable.
+#Print the value of the variable as the output
+ 
+#Expected outcome:
+#Whatever the value of the variable was, it will be printed as the output
+
+
+
+age =18
+print(age)
